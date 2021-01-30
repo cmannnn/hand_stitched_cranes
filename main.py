2425ed693047ce680a7c5f3780ce15e9a79aa839
Chris Murphy <cmurphy1465@gmail.com>
2021-01-30 11:32:40 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template
+
+# create a flask instance
+app = Flask(__name__)
+
+# create a route decorator
+@app.route('/')
+
+#def index():
+#	return "<h1>hand stitched cranes</h1>"
+
+def index():
+	first_name = 'chris'
+	stuff = 'a <strong>chinese</strong> inspired blog'
+	favorite_pizza = ['peppers', 'pineapple', 'mushroom', 'pesto']
+	return render_template('index.html', 
+		first_name=first_name,
+		stuff=stuff,
+		favorite_pizza=favorite_pizza)
+
+# localhost:50000/user/chris
+@app.route('/user/<name>')
+
+def user(name):
+	return render_template('user.html', user_name=name)
+
+# create custom error pages
+
+# invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'), 404
+
+# internal server error URL
+@app.errorhandler(500)
+def page_not_found(e):
+	return render_template('500.html'), 500
