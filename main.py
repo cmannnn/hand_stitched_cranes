094023b154ce4c7ab2f338062e3a1f0c1759bee2
Chris Murphy <cmurphy1465@gmail.com>
2021-02-15 10:06:00 -0500

    import wtforms

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # create a flask instance
 app = Flask(__name__)
