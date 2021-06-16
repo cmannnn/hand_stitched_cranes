cad407212619a8cce5275a28ac167336972923a0
cmannnn <cmurphy1465@gmail.com>
2021-06-15 21:20:48 -0400

    delete user button bug fix

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 # mySQL db
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:xxx@localhost/our_users'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:xxxx@localhost/our_users'
 
 # creating secret key HIDE
 app.config['SECRET_KEY'] = "****"
