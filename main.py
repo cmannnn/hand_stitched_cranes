26cfa533f3ae32d7f6fbf588acd4afc1268e8714
cmannnn <cmurphy1465@gmail.com>
2021-06-16 20:57:58 -0400

    hash password

--- main.py
+++ main.py
@@ -131,7 +131,9 @@ def add_user():
 	if form.validate_on_submit():
 		user = Users.query.filter_by(email = form.email.data).first()
 		if user is None:
-			user = Users(name = form.name.data, email = form.email.data, favorite_color = form.favorite_color.data, password_hash=form.password_hash.data)
+			# hash password
+			hashed_pw = generate_password_hash(form.password_hash.data, "sha256")
+			user = Users(name = form.name.data, email = form.email.data, favorite_color = form.favorite_color.data, password_hash=hashed_pw)
 			db.session.add(user)
 			db.session.commit()
 		name = form.name.data
