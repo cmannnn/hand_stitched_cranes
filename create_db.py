a637bac99a33d1869a267b501504f2aadbbab95b
cmannnn <cmurphy1465@gmail.com>
2021-06-22 19:40:01 -0400

    database backend

--- create_db.py
+++ create_db.py
@@ -10,7 +10,7 @@ my_cursor = mydb.cursor()
 
 #my_cursor.execute("CREATE DATABASE our_users")
 
-my_cursor.execute("SHOW DATABASES")
-for db in my_cursor:
-	print(db)
+#my_cursor.execute("SHOW DATABASES")
+#for db in my_cursor:
+#	print(db)
 
